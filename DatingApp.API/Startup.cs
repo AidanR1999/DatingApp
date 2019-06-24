2e0052fd779346ec94ed3536a51efef9866567a0
Aidan Rooney <AidanR1999@hotmail.com>
2019-06-24 23:13:44 +0100

    added the auth repos as a scoped service

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
 
         }
 
